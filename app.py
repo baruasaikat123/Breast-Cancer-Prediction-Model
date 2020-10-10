6ff28d595384e727daf3d6b86bdc266fbd28670d
Saikat <mrbarua123@gmail.com>
2020-10-10 12:14:58 +0530

    complete

---
+++ app.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    input_features = [float(x) for x in request.form.values()]
+    features_value = [np.array(input_features)]
+    
+    features_name = ['radius_mean', 'perimeter_mean', 'area_mean', 'compactness_mean',
+       'concavity_mean', 'concave points_mean', 'radius_se',
+       'perimeter_se', 'area_se', 'radius_worst',
+       'perimeter_worst', 'area_worst', 'compactness_worst', 'concavity_worst',
+       'concave points_worst']
+    
+    df = pd.DataFrame(features_value, columns=features_name)
+    output = model.predict(df)
+        
+    
+
+    return render_template('result.html', prediction=output)
+
+if __name__ == "__main__":
+    app.run()
